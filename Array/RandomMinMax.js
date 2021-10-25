58d1d7641ea5a35dd527961c68c11d151b853134
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-10-25 12:54:05 +0530

    Second max and min random number without sorting

--- Array/RandomMinMax.js
+++ Array/RandomMinMax.js
@@ -1,1 +1,14 @@
 console.log("Welcome to the Array Javascript problem ")
+
+let random = new Array();
+for (let i = 0; i < 10; i++) 
+{
+    random[i] = Math.floor(Math.random() * 899) + 100;
+}
+console.log(random);
+var maximum=Math.max.apply(null, random);
+var minimum=Math.min.apply(null, random);
+random.splice(random.indexOf(maximum), 1);
+random.splice(random.indexOf(minimum), 1);
+console.log("The second_Largest Number  is " + Math.max.apply(null, random));
+console.log("The second_Smallest Number is " + Math.min.apply(null, random));
